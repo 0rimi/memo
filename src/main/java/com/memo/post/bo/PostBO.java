f73876082cc248b58ca587cb56895d5ac562c9bc
DESKTOP-4SVTHBM\내문서 <0rm7698@gmail.com>
2023-01-17 18:39:30 +0900

    이미지 업로드

--- src/main/java/com/memo/post/bo/PostBO.java
+++ src/main/java/com/memo/post/bo/PostBO.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.memo.common.FileManagerService;
 import com.memo.post.dao.PostDAO;
 
 @Service
@@ -12,12 +13,18 @@ public class PostBO {
 	@Autowired
 	private PostDAO postDAO;
 	
+	@Autowired
+	private FileManagerService fileManagerService;
+	
 	public int addPost(int userId, String userLoginId,
 			String subject, String content, MultipartFile file) {
 		
 		// 파일 업로드 > 경로
 		String imgPath = null;
-				
+		if(file != null) {	// 파일이 있으면,
+			imgPath = fileManagerService.saveFile(userLoginId, file);
+		}
+		
 		return postDAO.insertPost(userId, subject, content, imgPath); 
 	}
 	
